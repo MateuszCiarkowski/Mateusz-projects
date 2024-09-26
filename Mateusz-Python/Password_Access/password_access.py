3ec32c316adb23559cc9822b9163353980e44038
MateuszCiarkowski <M.Ciarkowski2445@student.leedsbeckett.ac.uk>
2024-09-26 16:56:05 +0100

    Code trimmed, 'if' removed, code is not running through print(Incorrect..) while password is correct anymore. User_login and password preset as None to let 'while' start running.

---
+++ Mateusz-Python/Password_Access/password_access.py
@@ -0,0 +1,11 @@
+user_login = None
+user_password = None
+
+while user_login != 'x' or user_password != 'y':
+    print('Incorrect Login or Password', 'Try again')
+    user_login = input('Login: ')
+    user_password = input('Password: ')
+else:
+    print('You are logged in')
+    file = open('Computer Programming Exam Topics.txt', 'r')
+    print(file.read())
