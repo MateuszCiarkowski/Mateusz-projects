587341c86f6915fbbbf1d0f122145cd9126290df
MateuszCiarkowski <M.Ciarkowski2445@student.leedsbeckett.ac.uk>
2024-09-27 17:51:39 +0100

    \n

--- Mateusz-Python/Password_Access/password_access.py
+++ Mateusz-Python/Password_Access/password_access.py
@@ -2,7 +2,7 @@ user_login = None
 user_password = None
 
 while user_login != 'x' or user_password != 'y':
-    print('Incorrect Login or Password', 'Try again')
+    print('Incorrect Login or Password\nTry again')
     user_login = input('Login: ')
     user_password = input('Password: ')
 else:
