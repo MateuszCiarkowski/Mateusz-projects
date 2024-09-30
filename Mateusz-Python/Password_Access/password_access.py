8fb71bf4c6ae26be55ef57140e761a955546bca9
MateuszCiarkowski <M.Ciarkowski2445@student.leedsbeckett.ac.uk>
2024-09-30 21:39:30 +0100

    update

--- Mateusz-Python/Password_Access/password_access.py
+++ Mateusz-Python/Password_Access/password_access.py
@@ -1,11 +1,13 @@
 user_login = None
 user_password = None
 
-while user_login != 'x' or user_password != 'y':
-    print('Incorrect Login or Password\nTry again')
+while __name__ == "__main__":
     user_login = input('Login: ')
     user_password = input('Password: ')
-else:
-    print('You are logged in')
-    file = open('Computer Programming Exam Topics.txt', 'r')
-    print(file.read())
+    if user_login == 'x' and user_password == 'y':
+        print('You are logged in')
+        file = open('Computer Programming Exam Topics.txt', 'r')
+        print(file.read())
+        break
+    else:
+        print('Incorrect Login or Password\nTry again')
