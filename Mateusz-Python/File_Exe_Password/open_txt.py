b373bb3c6863645bb7f5022e54b4d8df38073304
MateuszCiarkowski <M.Ciarkowski2445@student.leedsbeckett.ac.uk>
2024-09-30 02:54:25 +0100

    no repeating print

--- Mateusz-Python/File_Exe_Password/open_txt.py
+++ Mateusz-Python/File_Exe_Password/open_txt.py
@@ -3,13 +3,15 @@ import subprocess
 user_login = None
 user_password = None
 
-while user_login != 'x' or user_password != 'y':
-    print('Incorrect Login or Password\nTry again')
+while __name__ == "__main__":
     user_login = input('Login: ')
     user_password = input('Password: ')
-else:
-    print('You are logged in')
-    subprocess.call(['C:\\Windows\\system32\\notepad.exe', 'Computer Programming Exam Topics.txt'])
+    if user_login == 'x' and user_password == 'y':
+        print('You are logged in')
+        subprocess.call(['notepad', 'Computer Programming Exam Topics.txt'])
+        break
+    else:
+        print('Incorrect Login or Password\nTry again')
 
 
 
